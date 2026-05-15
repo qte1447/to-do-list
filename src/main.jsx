6b048d5d674c0d3f35073992ab94238804fbb7bf
qte1447 <101118408+qte1447@users.noreply.github.com>
2026-05-16 01:13:12 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import './apiBypass';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
+import './apiBypass.js';
 
 // монтируем приложение в div с id="root" из index.html
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
