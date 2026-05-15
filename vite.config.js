981d7e7c6fe32599e48bbbc5a4f00765017e4980
qte1447 <101118408+qte1447@users.noreply.github.com>
2026-05-15 23:16:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/to-do-list/', //aaaaa
   plugins: [react()],
-  base: '/to-do-list/', // woaaaaaah maaaan
 })
 
