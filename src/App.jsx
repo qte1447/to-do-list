d923dfbe4aca2fd434548fd80037502b140a9445
qte1447 <101118408+qte1447@users.noreply.github.com>
2026-08-03 23:42:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -141,6 +141,8 @@ function App() {
         {!loading && !error && (
           <>
             <div className='info'>
+              <div className="stock-placard">
+              </div>
               <div className='money'>
                 <div id="USD">
                   Доллар США $ — {rates.USDrate} руб.
